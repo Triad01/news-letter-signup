bb0980c25706feef25f8036f33010dc408352106
Triad01 <luckypee01@gmail.com>
2023-08-05 16:13:25 +0100

    completed final work

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+const form = document.querySelector(".section__form");
+const emailInput = document.querySelector(".email__input");
+const validationText = document.querySelector(".validation__text");
+
+// validating upon form submission=======
+form.addEventListener("submit", () => {
+  const validEmail = emailInput.value.includes("@");
+  if (validEmail) {
+    window.location.href = "/success.html";
+  } else {
+    emailInput.classList.add("invalid");
+    validationText.style.display = "block";
+  }
+});
+
+//validating upon input change and possibly input blur
---
+++ app2.js
@@ -0,0 +1,6 @@
+"use-strict";
+const dismissBtn = document.querySelector("#dismiss__btn");
+
+dismissBtn.addEventListener("click", () => {
+  window.location.href = "/index.html";
+});
